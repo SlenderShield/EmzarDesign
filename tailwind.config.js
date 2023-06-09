2b57755d0a5d8b9342cc616ad9f989c453913bc6
Muralidhara Bhat KS <ksmuralidhara0@gmail.com>
2023-06-09 23:03:22 +0530

    tailwind config update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,22 @@ module.exports = {
     purge: ['./index.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
     theme: {
         extend: {},
+        theme: {
+            screens: {
+                'sm': '640px',
+                // => @media (min-width: 640px) { ... } 
+
+                'md': '768px',
+                // => @media (min-width: 768px) { ... }
+                'mlg': '960px',
+
+                'lg': '1024px',
+                // => @media (min-width: 1024px) { ... }
+
+                'xl': '1280px',
+                // => @media (min-width: 1280px) { ... }
+            }
+        }
     },
     plugins: [],
 }
