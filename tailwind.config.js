31290e227443cfc1117776a2d90331368065e96e
Muralidhara Bhat KS <ksmuralidhara0@gmail.com>
2023-06-13 19:21:13 +0530

    fix bug asp.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 // tailwind.config.js
 module.exports = {
     mode: 'jit',
-    purge: ['./index.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
+    content: ['./index.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
     theme: {
         extend: {},
         theme: {
