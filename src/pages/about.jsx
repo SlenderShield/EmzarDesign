f1df7f99233c6b5f85223603a655edf11465096e
Muralidhara Bhat KS <ksmuralidhara0@gmail.com>
2023-06-13 21:24:24 +0530

    design change

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,68 +1,62 @@
 import React from "react";
 import Head from "../components/Head";
+
 const AboutUsPage = () => {
   return (
     <div className="container mx-auto px-4 py-8">
       <Head
         pageTitle="About Us"
         backgroundImage="/public/assets/master_bedroom/MBTwo.png"
-        pageDescription="The company you can relay on."
+        pageDescription="The company you can rely on."
       />
 
-      <div className="w-3/4 mx-auto mb-8">
-        <div className="mb-8">
-          <h2 className="text-2xl font-bold mb-4">Company Details</h2>
-          <p className="text-gray-700">
-            <strong>Company Name:</strong> ABC Interiors
+      <div className="mx-auto w-2/3 mt-10">
+        <div className="mb-8 ">
+          <h2 className="text-3xl font-bold mb-5">
+            <span className="border-b-2 border-blue-400 pb-1">
+              Company Details :
+            </span>
+          </h2>
+          <p className="text-white text-justify mb-3">
+            With expertise in both residential and commercial projects, we are
+            dedicated to delivering inspired designs that go above and beyond
+            our clients' expectations. Our team of talented designers is
+            committed to understanding your individual preferences and
+            requirements, ensuring that every detail is carefully considered and
+            incorporated into the design.
           </p>
-          <p className="text-gray-700">
-            <strong>Location:</strong> City, State, Country
+          <p className="text-white text-justify mb-3">
+            At Emzar Designs, we believe that a well-designed space has the
+            power to transform lives. Whether it's your dream home or a
+            professional workspace, we strive to create interiors that are not
+            only aesthetically pleasing but also highly functional. Our
+            meticulous attention to detail and use of premium materials ensure
+            that each project is of the highest quality.
           </p>
-          <p className="text-gray-700">
-            <strong>Contact:</strong> +1 123 456 7890
-          </p>
-          <p className="text-gray-700">
-            <strong>Email:</strong> info@abcinteriors.com
-          </p>
-        </div>
-
-        <div>
-          <h2 className="text-2xl font-bold mb-4">About Our Company</h2>
-          <p className="text-gray-700">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
-            vehicula erat ac nisi ultricies, in lobortis nunc interdum. Morbi
-            consectetur laoreet quam, at vulputate dolor tincidunt a. Nullam ut
-            imperdiet tortor. Aliquam erat volutpat. Nulla facilisi. Donec
-            scelerisque nunc quis ante feugiat tempus. Aenean lobortis viverra
-            justo vitae vestibulum. Morbi id placerat justo. Phasellus ut semper
-            ligula. Fusce rhoncus odio eget neque dapibus placerat.
-          </p>
-          <p className="text-gray-700 mt-2">
-            Vestibulum nec lacinia sem. Fusce dignissim, dui non suscipit
-            interdum, nunc lectus feugiat ipsum, eu cursus justo lectus in nunc.
-            Donec auctor facilisis ipsum, a vulputate nulla pulvinar eu. Nulla a
-            risus tellus. Sed nec gravida elit. Mauris lobortis rutrum purus,
-            non blandit lectus ullamcorper et. Proin eget feugiat justo, id
-            scelerisque mi. Nunc placer
+          <p className="text-white text-justify mb-3">
+            With our focus on simplicity, we aim to make the entire design
+            process smooth and enjoyable for our clients. From the initial
+            concept to the final execution, we work closely with you, providing
+            clear communication and regular updates, ensuring that your vision
+            comes to life seamlessly.
           </p>
         </div>
-
-        {/* Add more cool things about the company */}
-        <div className="flex flex-col md:flex-row items-center mb-8">
-          <img
-            src="/assets/ayesha.jpg"
-            alt="Owner"
-            className="w-64 h-64 rounded-full mb-4 md:mb-0 md:mr-8"
-          />
-          <div>
-            <h2 className="text-2xl font-bold mb-2">John Doe</h2>
-            <p className="text-gray-600">Founder & CEO</p>
-            <p className="text-gray-700 mt-2">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
-              vehicula erat ac nisi ultricies, in lobortis nunc interdum. Morbi
-              consectetur laoreet quam, at vulputate dolor tincidunt a. Nullam
-              ut imperdiet tortor. Aliquam erat volutpat. Nulla facilisi.
-            </p>
+      </div>
+      <div className="flex justify-center">
+        <div className="w-70">
+          <h2 className="text-xl font-bold mb-2 text-center">
+            <span className="border-b-2 border-blue-400 pb-1">Our team :</span>
+          </h2>
+          <div className="bg-white rounded-lg p-4 shadow-md">
+            <div className="flex justify-center">
+              <img
+                src="team-member.jpg"
+                alt="Team Member"
+                className="w-48 h-48 object-cover rounded-lg"
+              />
+            </div>
+            <h3 className="text-lg font-bold mb-2 text-center">John Doe</h3>
+            <p className="text-gray-600 text-center">Founder & CEO</p>
           </div>
         </div>
       </div>
