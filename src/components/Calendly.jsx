982387580a7aa06336123ca7aed99e28adfb3dcb
Muralidhara Bhat KS <ksmuralidhara0@gmail.com>
2023-06-13 21:24:57 +0530

    meet link component

---
+++ src/components/Calendly.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const CalendlyButton = ({ link, description, height }) => {
+  const handleRedirect = () => {
+    window.open(link, "_blank");
+  };
+  const buttonStyle = {
+    position: "fixed",
+    bottom: `${height}px`,
+    right: "8px",
+  };
+
+  return (
+    <div style={buttonStyle}>
+      <button
+        className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-full shadow"
+        onClick={handleRedirect}
+        formTarget="_blank"
+      >
+        {description}
+      </button>
+    </div>
+  );
+};
+
+export default CalendlyButton;
