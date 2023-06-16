dc6a2680024307b9f6db2e45a97fa3946c1e9e90
Muralidhara Bhat KS <ksmuralidhara0@gmail.com>
2023-06-16 13:15:13 +0530

    naming fix

--- src/images.js
+++ src/images.js
@@ -10,7 +10,7 @@ import kitchenFront from '../assets/SNN_Clermont_Apartment/kitchen_front.jpg';
 import kitchenSide from '../assets/SNN_Clermont_Apartment/kitchen_side.jpg';
 import kitchenSideTwo from '../assets/SNN_Clermont_Apartment/kitchen_side_two.jpg';
 import tvRoomFront from '../assets/SNN_Clermont_Apartment/tv_room_front.jpg';
-import tvRoomSide from '../assets/SNN_Clermont_Apartment/tv_room_side.jpg';
+import tvRoomSideSNN from '../assets/SNN_Clermont_Apartment/tv_room_side.jpg';
 
 import bedroomSideView from '../assets/Srikara_Enclave/Bedroom_side_view.jpg';
 import tvRoom from '../assets/Srikara_Enclave/TV_room.jpg';
@@ -20,7 +20,7 @@ import hall from '../assets/Srikara_Enclave/hall.jpg';
 import hallSide from '../assets/Srikara_Enclave/hall_side.jpg';
 import livingRoom from '../assets/Srikara_Enclave/living_room.jpg';
 import tvFront from '../assets/Srikara_Enclave/tv_front.jpg';
-import tvRoomSide from '../assets/Srikara_Enclave/tv_room_side.jpg';
+import tvRoomSideSri from '../assets/Srikara_Enclave/tv_room_side.jpg';
 
 import gamingView from '../assets/Study_Gaming_Room/gaming_view.jpg';
 import sofaSet from '../assets/Study_Gaming_Room/sofa_set.jpg';
@@ -47,7 +47,7 @@ const images = {
         kitchen_side: kitchenSide,
         kitchen_side_two: kitchenSideTwo,
         tv_room_front: tvRoomFront,
-        tv_room_side: tvRoomSide,
+        tv_room_side: tvRoomSideSNN,
     },
     'Srikara_Enclave': {
         Bedroom_sideview: bedroomSideView,
@@ -58,7 +58,7 @@ const images = {
         hall_side: hallSide,
         living_room: livingRoom,
         tv_front: tvFront,
-        tv_room_side: tvRoomSide,
+        tv_room_side: tvRoomSideSri,
     },
     'Study_Gaming_Room': {
         gaming_view: gamingView,
