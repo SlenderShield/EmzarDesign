a1a50cffbda6b2cf1304cf40494358a52e367fe4
Muralidhara Bhat KS <ksmuralidhara0@gmail.com>
2023-06-16 13:13:06 +0530

    img import fix

--- src/images.js
+++ src/images.js
@@ -1,43 +1,76 @@
+import mbSideView from '../assets/master_bedroom/mb_side_view.jpg';
+import mbSideWide from '../assets/master_bedroom/mb_side_wide.jpg';
+import mbTv from '../assets/master_bedroom/mb_tv.jpg';
+import mbTvSide from '../assets/master_bedroom/mb_tv_side.jpg';
+import mbView from '../assets/master_bedroom/mb_view.jpg';
+
+import dining from '../assets/SNN_Clermont_Apartment/dining.jpg';
+import foyer from '../assets/SNN_Clermont_Apartment/foyer.jpg';
+import kitchenFront from '../assets/SNN_Clermont_Apartment/kitchen_front.jpg';
+import kitchenSide from '../assets/SNN_Clermont_Apartment/kitchen_side.jpg';
+import kitchenSideTwo from '../assets/SNN_Clermont_Apartment/kitchen_side_two.jpg';
+import tvRoomFront from '../assets/SNN_Clermont_Apartment/tv_room_front.jpg';
+import tvRoomSide from '../assets/SNN_Clermont_Apartment/tv_room_side.jpg';
+
+import bedroomSideView from '../assets/Srikara_Enclave/Bedroom_side_view.jpg';
+import tvRoom from '../assets/Srikara_Enclave/TV_room.jpg';
+import diningArea from '../assets/Srikara_Enclave/dining_area.jpg';
+import diningView from '../assets/Srikara_Enclave/Dining_View.jpg';
+import hall from '../assets/Srikara_Enclave/hall.jpg';
+import hallSide from '../assets/Srikara_Enclave/hall_side.jpg';
+import livingRoom from '../assets/Srikara_Enclave/living_room.jpg';
+import tvFront from '../assets/Srikara_Enclave/tv_front.jpg';
+import tvRoomSide from '../assets/Srikara_Enclave/tv_room_side.jpg';
+
+import gamingView from '../assets/Study_Gaming_Room/gaming_view.jpg';
+import sofaSet from '../assets/Study_Gaming_Room/sofa_set.jpg';
+import tvStation from '../assets/Study_Gaming_Room/tv_station.jpg';
+import wideSofa from '../assets/Study_Gaming_Room/wide_sofa.jpg';
+import wideView from '../assets/Study_Gaming_Room/wide_view.jpg';
+
+import feature1 from '../assets/feature1.png';
+import logo from '../assets/logo.jpg';
+import ayesha from '../assets/ayesha.jpg';
+
 const images = {
     'Master Bedroom': {
-        mb_side_view: require('../assets/master_bedroom/mb_side_view.jpg').default,
-        mb_side_wide: require('../assets/master_bedroom/mb_side_wide.jpg').default,
-        mb_tv: require('../assets/master_bedroom/mb_tv.jpg').default,
-        mb_tv_side: require('../assets/master_bedroom/mb_tv_side.jpg').default,
-        mb_view: require('../assets/master_bedroom/mb_view.jpg').default,
+        mb_side_view: mbSideView,
+        mb_side_wide: mbSideWide,
+        mb_tv: mbTv,
+        mb_tv_side: mbTvSide,
+        mb_view: mbView,
     },
     'SNN_Clermont_Apartment': {
-        dining: require('../assets/SNN_Clermont_Apartment/dining.jpg').default,
-        foyer: require('../assets/SNN_Clermont_Apartment/foyer.jpg').default,
-        kitchen: require('../assets/SNN_Clermont_Apartment/kitchen.jpg').default,
-        kitchen_side_two: require('../assets/SNN_Clermont_Apartment/kitchen_side_two.jpg').default,
-        kitchen_front: require('../assets/SNN_Clermont_Apartment/kitchen_front.jpg').default,
-        kitchen_side: require('../assets/SNN_Clermont_Apartment/kitchen_side.jpg').default,
-        tv_room_front: require('../assets/SNN_Clermont_Apartment/tv_room_front.jpg').default,
-        tv_room_side: require('../assets/SNN_Clermont_Apartment/tv_room_side.jpg').default,
+        dining,
+        foyer,
+        kitchen_front: kitchenFront,
+        kitchen_side: kitchenSide,
+        kitchen_side_two: kitchenSideTwo,
+        tv_room_front: tvRoomFront,
+        tv_room_side: tvRoomSide,
     },
     'Srikara_Enclave': {
-        Bedroom_sideview: require('../assets/Srikara_Enclave/Bedroom_side_view.jpg').default,
-        dining_area: require('../assets/Srikara_Enclave/dining_area.jpg').default,
-        dining_view: require('../assets/Srikara_Enclave/Dining_View.jpg').default,
-        hall: require('../assets/Srikara_Enclave/hall.jpg').default,
-        hall_side: require('../assets/Srikara_Enclave/hall_side.jpg').default,
-        living_room: require('../assets/Srikara_Enclave/living_room.jpg').default,
-        tv_front: require('../assets/Srikara_Enclave/tv_front.jpg').default,
-        TV_room: require('../assets/Srikara_Enclave/TV_room.jpg').default,
-        tv_room_side: require('../assets/Srikara_Enclave/tv_room_side.jpg').default,
+        Bedroom_sideview: bedroomSideView,
+        TV_room: tvRoom,
+        dining_area: diningArea,
+        dining_view: diningView,
+        hall,
+        hall_side: hallSide,
+        living_room: livingRoom,
+        tv_front: tvFront,
+        tv_room_side: tvRoomSide,
     },
     'Study_Gaming_Room': {
-        gaming_view: require('../assets/Study_Gaming_Room/gaming_view.jpg').default,
-        sofa_set: require('../assets/Study_Gaming_Room/sofa_set.jpg').default,
-        tv_station: require('../assets/Study_Gaming_Room/tv_station.jpg').default,
-        wide_sofa: require('../assets/Study_Gaming_Room/wide_sofa.jpg').default,
-        wide_view: require('../assets/Study_Gaming_Room/wide_view.jpg').default,
+        gaming_view: gamingView,
+        sofa_set: sofaSet,
+        tv_station: tvStation,
+        wide_sofa: wideSofa,
+        wide_view: wideView,
     },
     'features': {
-        one: require('../assets/feature1.png').default,
-        logo: require('../assets/logo.jpg').default,
-        ceo: require('../assets/ayesha.jpg').default,
+        one: feature1,
+        logo,
+        ceo: ayesha,
     }
 };
 
