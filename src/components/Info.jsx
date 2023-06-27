cf5e492c8e65b76e3e2213c78121bc54c2e36a21
Muralidhara Bhat KS <ksmuralidhara0@gmail.com>
2023-06-27 20:33:21 +0530

    ui change

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -6,12 +6,15 @@ const Info = () => {
   return (
     <div className="flex justify-between items-center m-4 mx-10">
       {/* Mail Link */}
-      <div className="flex flex-col">
-        <a href="mailto:contact@example.com" className="text-white mb-2">
+      <div className="flex flex-col sm:flex-row ">
+        <a
+          href="mailto:contact@example.com"
+          className="text-white mb-2 sm:mr-2"
+        >
           <AiOutlineMail className="inline-block mr-1" />
           contact@example.com
         </a>
-        <a href="tel:6362037006" className="text-white hidden sm:block">
+        <a href="tel:6362037006" className="text-white hidden sm:block ">
           <AiTwotonePhone className="inline-block mb-1 mr-1" />
           6362037006
         </a>
