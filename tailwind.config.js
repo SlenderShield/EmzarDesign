9ce374d6b017aed354e866e0ecbd64dd066b46a8
Muralidhara Bhat KS <ksmuralidhara0@gmail.com>
2023-06-28 22:59:24 +0530

    change in screen breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,18 +6,12 @@ module.exports = {
         extend: {},
         theme: {
             screens: {
-                'sm': '640px',
-                // => @media (min-width: 640px) { ... } 
-
-                'md': '768px',
-                // => @media (min-width: 768px) { ... }
-                'mlg': '960px',
-
-                'lg': '1024px',
-                // => @media (min-width: 1024px) { ... }
-
-                'xl': '1280px',
-                // => @media (min-width: 1280px) { ... }
+                screens: {
+                    'sm': '576px',
+                    'md': '768px',
+                    'lg': '992px',
+                    'xl': '1024px',
+                },
             }
         }
     },
