86410187e1871881c846f7bfb46c03fa4843629b
Muralidhara Bhat KS <ksmuralidhara0@gmail.com>
2023-07-04 00:33:48 +0530

    color changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,22 @@ module.exports = {
     mode: 'jit',
     content: ['./index.html', './**/*.{js,jsx,ts,tsx,vue}'],
     theme: {
-        extend: {},
-        theme: {
+        extend: {
             screens: {
-                screens: {
-                    'sm': '576px',
-                    'md': '768px',
-                    'lg': '992px',
-                    'xl': '1024px',
-                },
-            }
-        }
+                'sm': '576px',
+                'md': '768px',
+                'lg': '992px',
+                'xl': '1024px',
+            },
+            colors: {
+                // Custom colors
+                'primary_color': '#0F6083',
+                'secondary_color': '#418689',
+                'accent_color': '#2ca1b1',
+                'background_color': '#f8f8f9',
+                'sec_bg_color': '#b0e4eb'
+            },
+        },
     },
     plugins: [],
-}
+};
