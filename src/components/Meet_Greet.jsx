6b000461196d1786e35d96f573c077a48a5ce581
Muralidhara Bhat KS <ksmuralidhara0@gmail.com>
2023-07-04 19:53:09 +0530

    test

--- src/components/Meet_Greet.jsx
+++ src/components/Meet_Greet.jsx
@@ -13,7 +13,7 @@ const ContactButton = ({ link, description, height }) => {
   return (
     <div style={buttonStyle}>
       <button
-        className="bg-accent_color hover:bg-primary_color text-white px-4 py-2 rounded-full shadow border-none"
+        className="bg-[#051641] hover:bg-primary_color text-white px-4 py-2 rounded-full shadow border-none"
         onClick={handleRedirect}
         formTarget="_blank"
       >
