333cc05fb5cfadaf61390e01fb4093cf03a13409
Muralidhara Bhat KS <ksmuralidhara0@gmail.com>
2023-07-04 19:49:27 +0530

    testing

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -7,7 +7,7 @@ const CallToAction = ({ title, description, link }) => {
       <div className="flex justify-center m-5">
         <Link
           to={link}
-          className="bg-primary_color hover:bg-white hover:text-primary_color text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out flex items-center"
+          className="bg-[#051641] hover:bg-white hover:text-primary_color text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out flex items-center"
         >
           {description}
         </Link>
